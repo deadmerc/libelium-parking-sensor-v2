8093fd9c13c9b0b14bb5bb59f14f3cb80750cedc
Max <2972866+deadmerc@users.noreply.github.com>
2025-12-13 15:16:49 +0400

    bump version to 0.1.2

--- src/libelium_parking_sensor_v2/__init__.py
+++ src/libelium_parking_sensor_v2/__init__.py
@@ -2,5 +2,5 @@ from incremental import Version
 
 from .decoder import decode
 
-__version__ = Version("LibeliumParkingSensor", 0, 1, 1)
+__version__ = Version("LibeliumParkingSensor", 0, 1, 2)
 __all__ = ["decode", "__version__"]
